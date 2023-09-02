a7c223d8fc46593625f1b48bc0ceb816227ba22f
xutao <xutao@apexsoft.com.cn>
2023-09-02 23:37:30 +0800

    修改前缀树

--- src/main/java/com/hai/tang/algorithm/TrieTree.java
+++ src/main/java/com/hai/tang/algorithm/TrieTree.java
@@ -1,6 +1,8 @@
 package com.hai.tang.algorithm;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,23 +18,25 @@ public class TrieTree {
     /**
      * 根节点
      */
-    private Node root;
+    private Node root = new Node(new HashMap<>(), false);
 
-
-    /**
-     * 当前节点的下一个节点
-     */
-    // private Map<Character, Node> nextNode = new HashMap<>();
-    private Map<Integer, Map<Character, Node>> everyLeverNode = new HashMap<>();
 
 
     public class Node {
-        public Character value;
         public Map<Character, Node> childNodeMap;
+        public boolean isWord;
 
-        public Node(Character value, Map<Character, Node> childNodeMap) {
-            this.value = value;
+        private Node(Map<Character, Node> childNodeMap, boolean isWord) {
             this.childNodeMap = childNodeMap;
+            this.isWord = isWord;
+        }
+
+        public void setIsWord(boolean isWord) {
+            this.isWord = isWord;
+        }
+
+        public boolean getIsWord() {
+            return isWord;
         }
     }
 
@@ -48,53 +52,138 @@ public class TrieTree {
      */
     public void add(String addValue) {
         char[] charArray = addValue.toCharArray();
-        if (null == root) {
-            //设置根节点里的值为空字符串
-            root = new Node('a', null);
-        }
+        Map<Character, Node> fatherNodeMap = root.childNodeMap;
         for (int i = 0; i < charArray.length; i++) {
             char ch = charArray[i];
-            if (null != root) {
-                if (null == root.childNodeMap) {
-                    Map<Character, Node> firstLeverMap = new HashMap<>();
-                    firstLeverMap.put(ch, null);
-                    root.childNodeMap = firstLeverMap;
-
-//                    everyLeverNode.put(1, firstLeverMap);
-//                    size++;
+            if (fatherNodeMap.containsKey(ch)) {
+                Node node = fatherNodeMap.get(ch);
+                if (i != charArray.length - 1) {
+                    if (node.childNodeMap == null) {
+                        node.childNodeMap = new HashMap<>();
+                    }
+                    fatherNodeMap = node.childNodeMap;
                 } else {
-                    add(ch, root.childNodeMap, charArray, 1, i + 1);
-                }
-            }
-        }
-    }
-
-
-    public void add(Character ch, Map<Character, Node> currentLeverMap, char[] charArray, int currentLever, int level) {
-        if (null != currentLeverMap) {
-            //当前层级不包含则加上
-            if (currentLever == level) {
-                if (!currentLeverMap.containsKey(ch)) {
-                    currentLeverMap.put(ch, null);
-//                    everyLeverNode.put(currentLever, currentLeverMap);
-//                    size++;
-                }
-            }
-            if (currentLever < level) {
-                char currentCh = charArray[currentLever - 1];
-                if (currentLeverMap.containsKey(currentCh)) {
-                    Node node = currentLeverMap.get(currentCh);
-                    if (node == null) {
-                        currentLeverMap.put(currentCh, new Node(null , new HashMap<>()));
-                        everyLeverNode.put(currentLever + 1, currentLeverMap);
+                    if (!node.getIsWord()) {
+                        node.setIsWord(true);
                     }
                 }
-                add(ch, currentLeverMap.get(currentCh).childNodeMap, charArray, currentLever + 1, level);
+            } else {
+                if (i != charArray.length - 1) {
+                    Node nodeNew = new Node(new HashMap<>(), false);
+                    fatherNodeMap.put(ch, nodeNew);
+                    fatherNodeMap = nodeNew.childNodeMap;
+                } else {
+                    Node nodeNew = new Node(null, true);
+                    fatherNodeMap.put(ch, nodeNew);
+                }
             }
         }
     }
 
-    public Node get() {
+
+    public Node getRoot() {
         return root;
+    }
+
+
+    public List<String> getMatchStr(String inputStr) {
+        List<String> matches = new ArrayList<>();
+        if (null == root.childNodeMap) {
+            return matches;
+        }
+        Node node = havaWord(inputStr, root, matches);
+        if (node != null) {
+            getMatchStr(new StringBuilder(inputStr), new StringBuilder(""), matches, node.childNodeMap);
+        }
+        return matches;
+    }
+
+    private void getMatchStr(StringBuilder inputStr, StringBuilder afterStr, List<String> matches, Map<Character, Node> childNodeMap) {
+        if (childNodeMap != null) {
+            for (Map.Entry<Character, Node> m : childNodeMap.entrySet()) {
+                Character key = m.getKey();
+                StringBuilder afterStr1 = new StringBuilder(afterStr);
+                afterStr1.append(key);
+
+                Node node = m.getValue();
+                boolean isWord = node.getIsWord();
+                StringBuilder inputStrOriginl = new StringBuilder(inputStr);
+                if (isWord) {
+                    matches.add(inputStrOriginl.append(afterStr1).toString());
+                }
+
+                if (node.childNodeMap != null) {
+                    if (!isWord) {
+                        getMatchStr(inputStrOriginl.append(afterStr1), new StringBuilder(""), matches, node.childNodeMap);
+                    } else {
+                        getMatchStr(inputStrOriginl, new StringBuilder(""), matches, node.childNodeMap);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 判断前缀树上的所有词语，是否有等于 str 的
+     */
+    public boolean equelWord(String str) {
+        if (null == str) {
+            return false;
+        }
+        List<String> list = new ArrayList<>();
+        Node node = havaWord(str, root, list);
+        if (node != null && list.size() > 0 && str.equals(list.get(0))) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 判断前缀树上的所有词语，是否有 str 开头的
+     */
+    public boolean startWordWith(String str) {
+        if (null == str) {
+            return false;
+        }
+        List<String> list = new ArrayList<>();
+        Node node = havaWord(str, root, list);
+        if (node != null) {
+            if (list.size() == 0) {
+                return true;
+            }
+            if (list.size() == 1 && str.equals(list.get(0))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 判断前缀树上是否有 以 inputStr 开头的词语，
+     * -有,则返回 inputStr 最后一个字符的子节点,若最后一个节点没有子节点则返回最后一个字符的节点
+     *      若返回的节点不为 null，且 matches 的大小为 0，则返回的是最后一个字符的子节点（即表明 inputStr 是前缀树上的一个词语的开头，如：前缀树上有 “一生一世” 这个词，但 inputStr 是 “一生一”）
+     *      若返回的节点不为 null，且 matches 的大小为 1，则返回的是 inputStr 倒数第二个字符的节点（即表明 inputStr 是前缀树上的一个词语，如：前缀树上有 “一生一世” 这个词，但 inputStr 是 “一生一世”）
+     * -没有,则返回 null
+     */
+    private Node havaWord(String inputStr, Node startNode, List<String> matches) {
+        char[] charArray = inputStr.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            char ch = charArray[i];
+            Map<Character, Node> childNodeMap = startNode.childNodeMap;
+            if (childNodeMap.containsKey(ch)) {
+                Node node = childNodeMap.get(ch);
+                if (node != null) {
+                    if (node.childNodeMap != null) {
+                        startNode = node;
+                    }
+                    if (null != matches && i == charArray.length - 1 && node.isWord) {
+                        matches.add(inputStr);
+                    }
+                }
+            } else {
+                return null;
+            }
+        }
+        return startNode;
     }
 }
--- src/test/java/com/hai/tang/algorithm/TrieTreeTest.java
+++ src/test/java/com/hai/tang/algorithm/TrieTreeTest.java
@@ -11,11 +11,17 @@ public class TrieTreeTest {
     @DisplayName("前缀树测试")
     public void trieTree() {
         TrieTree t = new TrieTree();
-        String[] array=new String[]{"张三","张五","张留的歌","张四","张四的","张四的歌","张四的歌啊","qqq","李四","李四的","李四的歌"};
+        //String[] array=new String[]{"张三","张五","张留的歌","张三的歌","张四","张四的","李四付出","张四的歌","李四的歌","张四的歌啊","qqq","李四","李四的","张五是到","李四的改"};
+        String[] array=new String[]{"张三","张五","张留的歌","张三的歌","张四","张四","李四付出","李四的歌","张四的歌啊","李四的歌啊","qqq","李四","李四","张五是到","李四的改"};
         for (String s : array) {
             t.add(s);
         }
-        System.out.println(t.get());
+        System.out.println(t.getRoot());
+       //  System.out.println(t.getMatchStr("张"));
+         //System.out.println(t.havaCompleteWord("张三"));
+         System.out.println(t.equelWord("张四的"));
+         System.out.println(t.startWordWith("张四的"));
+
     }
 
 }
