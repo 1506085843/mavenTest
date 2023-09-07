7067a3b45022c2a0d90edac9ebec03badd9191c3
xutao <1506085843@qq.com>
2023-09-07 16:27:32 +0800

    修改前缀树和前缀树的测试类

--- src/main/java/com/hai/tang/algorithm/TrieTree.java
+++ src/main/java/com/hai/tang/algorithm/TrieTree.java
@@ -44,14 +44,12 @@ public class TrieTree {
         }
     }
 
-    /**
-     * 获取前缀树的词语数
-     */
-    public Node getRootNode() {
-        return root;
-    }
 
-    public void clearRootNode() {
+
+    /**
+     * 清空前缀树
+     */
+    public void clear() {
         root = new Node(new HashMap<>(), false);
         size = 0;
     }
@@ -124,7 +122,7 @@ public class TrieTree {
     /**
      * 返回前缀树上以 inputStr 开头的词语
      */
-    public List<String> getMatchStr(String inputStr) {
+    public List<String> matchStartWord(String inputStr) {
         List<String> matches = new ArrayList<>();
         if (root.childNodeMap.size() == 0) {
             return matches;
@@ -235,11 +233,8 @@ public class TrieTree {
         }
         List<String> list = new ArrayList<>();
         Node node = havaWord(str, root, list);
-        if (node == null) {
-            return false;
-        }
         if (list.size() == 0) {
-            return true;
+            return node != null;
         }
         return list.size() == 1 && str.equals(list.get(0));
     }
@@ -247,8 +242,8 @@ public class TrieTree {
     /**
      * 判断前缀树上是否有 以 inputStr 开头的词语，
      * -有,则返回 inputStr 最后一个字符的子节点,若最后一个节点没有子节点则返回最后一个字符的节点
-     * 若返回的节点不为 null，且 matches 的大小为 0，则返回的是最后一个字符的子节点（即表明 inputStr 是前缀树上的一个词语的开头，如：前缀树上有 “一生一世” 这个词，但 inputStr 是 “一生一”）
-     * 若返回的节点不为 null，且 matches 的大小为 1，则返回的是 inputStr 倒数第二个字符的节点（即表明 inputStr 是前缀树上的一个词语，如：前缀树上有 “一生一世” 这个词，但 inputStr 是 “一生一世”）
+     *      若返回的节点不为 null，且 matches 的大小为 0，则返回的是最后一个字符的子节点（即表明 inputStr 是前缀树上的一个词语的开头，如：前缀树上有 “一生一世” 这个词，但 inputStr 是 “一生一”）
+     *      若返回的节点不为 null，且 matches 的大小为 1，则返回的是 inputStr 倒数第二个字符的节点（即表明 inputStr 是前缀树上的一个词语，如：前缀树上有 “一生一世” 这个词，但 inputStr 是 “一生一世”）
      * -没有,则返回 null
      */
     private Node havaWord(String inputStr, Node startNode, List<String> matches) {
@@ -258,11 +253,17 @@ public class TrieTree {
             Map<Character, Node> childNodeMap = startNode.childNodeMap;
             if (childNodeMap.containsKey(ch)) {
                 Node node = childNodeMap.get(ch);
-                if (node.childNodeMap != null) {
+                if (node.childNodeMap == null && i != charArray.length - 1) {
+                    return null;
+                }
+                if (node.childNodeMap != null || charArray.length == 1) {
                     startNode = node;
                 }
                 if (null != matches && i == charArray.length - 1 && node.isWord) {
                     matches.add(inputStr);
+                }
+                if (i == charArray.length - 1 && node.isWord && node.childNodeMap == null) {
+                    return null;
                 }
             } else {
                 return null;
--- src/test/java/com/hai/tang/algorithm/TrieTreeTest.java
+++ src/test/java/com/hai/tang/algorithm/TrieTreeTest.java
@@ -3,38 +3,113 @@ package com.hai.tang.algorithm;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
+import java.util.List;
 
 @DisplayName("前缀树测试类")
 public class TrieTreeTest {
 
+    @Test
+    @DisplayName("前缀树中增加词语")
+    public void add() {
+        String[] array = new String[]{"菜鸡","菜狗","张三","张三丰满", "张三丰", "张三丰是狗", "张全", "张力",  "张力懦夫","王五","王五王八", "王六滚蛋","滚蛋","滚"};
+        TrieTree trieTree = new TrieTree();
+        //将数组里面的所有词语加入到前缀树上
+        trieTree.add(array);
+    }
 
     @Test
-    @DisplayName("前缀树测试")
-    public void trieTree() {
-        TrieTree t = new TrieTree();
-        //String[] array=new String[]{"张三","张五","张留的歌","张三的歌","张四","张四的","李四付出","张四的歌","李四的歌","张四的歌啊","qqq","李四","李四的","张五是到","李四的改"};
-        String[] array = new String[]{"张三的歌","张三的曲","我爱你", "张三", "张力", "张五", "张留的歌", "张留", "李四付出", "李四的歌", "张四的歌啊", "李四的歌啊", "qqq", "李四", "李四", "张五是到", "李四的改"};
-        t.add(array);
-        //t.clearRootNode();
-         System.out.println(t.getRootNode());
+    @DisplayName("获取前缀树上以某个字符串开头的所有词语")
+    public void matchStartWord() {
+        String[] array = new String[]{"菜鸡","菜狗","张三","张三丰满", "张三丰", "张三丰是狗", "张全", "张力",  "张力懦夫","王五","王五王八", "王六滚蛋","滚蛋","滚"};
+        TrieTree trieTree = new TrieTree();
+        //将数组里面的所有词语加入到前缀树上
+        trieTree.add(array);
 
-        System.out.println(t.getMatchStr("张"));
+        //匹配前缀树上 "张" 开头的词语有哪些
+        List<String> matchResult = trieTree.matchStartWord("张");
+        //输出：[张全, 张三, 张三丰, 张三丰满, 张三丰是狗, 张力, 张力懦夫]
+        System.out.println(matchResult);
+    }
 
-        System.out.println(t.haveWord("张三"));
-        System.out.println(t.startWordWith("张三的"));
-        t.deleteWord("我爱你");//完全删除
-        System.out.println(t.getRootNode());
-        t.deleteWord("张三的曲");
-        System.out.println(t.getRootNode());
-        System.out.println(t.size());
+    @Test
+    @DisplayName("前缀树中是否有某个词语")
+    public void haveWord() {
+        String[] array = new String[]{"菜鸡","菜狗","张三","张三丰满", "张三丰", "张三丰是狗", "张全", "张力",  "张力懦夫","王五","王五王八", "王六滚蛋","滚蛋","滚"};
+        TrieTree trieTree = new TrieTree();
+        //将数组里面的所有词语加入到前缀树上
+        trieTree.add(array);
 
-        String text = "接着判断张三的歌张留指针2和指针3张留的张五李四的歌啊之间的字张四的歌啊符是否属于敏感词，假设指针2指向x，由于Root的子节点不存在x，\n" +
-                "那么指针3 = ++指针2，接着往后进行判断，当指针2走到a的时候，由于Root的子节点中存在a，qqq那么指针1指向字节点a，指\n" +
-                "针2不动，指针3往后走一步，指针3指向b，由于b是a的子节点，那么李四付出李四指针1指向节点b，指针3继续往后走，然后发现f不是节点\n";
-        System.out.println(t.sensitiveWordReplace(text));
-        System.out.println();
-        System.out.println(t.markWord("张三的歌张力，你今天过得怎么样张力", "【", "】"));
+        //判断前缀树上是否有 "张三" 这个词语
+        boolean existWord = trieTree.haveWord("张三");
+        System.out.println(existWord);  //true
+
+        //判断前缀树上是否有 "张三四" 这个词语
+        boolean existWord1 = trieTree.haveWord("张三四");
+        System.out.println(existWord1); //false
+    }
+
+    @Test
+    @DisplayName("判断前缀树上是否有某个字符串开头的词语")
+    public void haveStartWord() {
+        String[] array = new String[]{"菜鸡","菜狗","张三","张三丰满", "张三丰", "张三丰是狗", "张全", "张力",  "张力懦夫","王五","王五王八", "王六滚蛋","滚蛋","滚"};
+        TrieTree trieTree = new TrieTree();
+        //将数组里面的所有词语加入到前缀树上
+        trieTree.add(array);
+
+        //判断前缀树上是否有 "王六滚" 开头的词语
+        boolean haveStar = trieTree.startWordWith("王六滚");
+        System.out.println(haveStar); //true
+
+        //判断前缀树上是否有 "王六滚蛋" 开头的词语
+        boolean haveStar1 = trieTree.startWordWith("王六滚蛋");
+        System.out.println(haveStar1); //true
+    }
+
+    @Test
+    @DisplayName("从前缀树上删除某个词语")
+    public void deleteWord() {
+        String[] array = new String[]{"菜鸡","菜狗","张三","张三丰满", "张三丰", "张三丰是狗", "张全", "张力",  "张力懦夫","王五","王五王八", "王六滚蛋","滚蛋","滚"};
+        TrieTree trieTree = new TrieTree();
+        //将数组里面的所有词语加入到前缀树上
+        trieTree.add(array);
+
+
+        System.out.println("没删除前，前缀树含有"+trieTree.size()+"个词语");
+        trieTree.deleteWord("张三");
+        System.out.println("删除后，前缀树含有"+trieTree.size()+"个词语");
+        System.out.println("删除后，前缀树是否含有词语 \"张三\" ："+trieTree.haveWord("张三"));
+    }
+
+    @Test
+    @DisplayName("前缀树进行敏感词过滤")
+    public void sensitiveWordFilter() {
+        String[] array = new String[]{"菜鸡","菜狗","张三","张三丰满", "张三丰", "张三丰是狗", "张全", "张力",  "张力懦夫","王五","王五王八", "王六滚蛋","滚蛋","滚"};
+        TrieTree trieTree = new TrieTree();
+        //将数组里面的所有词语加入到前缀树上
+        trieTree.add(array);
+
+        String text = "小张打游戏很厉害，但是张三打游戏真菜鸡。上次我在游戏里说张三菜狗他就生气了。他说滚，我就滚滚蛋了。\n"+
+                "王五斗地主很厉害，上次王五王炸，张三丰就骂他，我们是队友你炸我干嘛，然后骂王五王八，王五听错了以为张三丰骂的，于是回骂张三丰是狗。\n"+
+                "然后场面一度失控，旁边的张力也被波及，什么张力懦夫张力滚蛋王五王八王五滚蛋都被乱七八糟的骂！";
+        //过滤后的文本
+        String textFilter = trieTree.sensitiveWordReplace(text);
+        System.out.println(textFilter);
+    }
+
+    @Test
+    @DisplayName("前缀树对文章、消息进行敏感词标记")
+    public void markWord() {
+        String[] array = new String[]{"菜鸡","菜狗","张三","张三丰满", "张三丰", "张三丰是狗", "张全", "张力",  "张力懦夫","王五","王五王八", "王六滚蛋","滚蛋","滚"};
+        TrieTree trieTree = new TrieTree();
+        //将数组里面的所有词语加入到前缀树上
+        trieTree.add(array);
+
+        String text = "小张打游戏很厉害，但是张三打游戏真菜鸡。上次我在游戏里说张三菜狗他就生气了。他说滚，我就滚滚蛋了。\n"+
+                "王五斗地主很厉害，上次王五王炸，张三丰就骂他，我们是队友你炸我干嘛，然后骂王五王八，王五听错了以为张三丰骂的，于是回骂张三丰是狗。\n"+
+                "然后场面一度失控，旁边的张力也被波及，什么张力懦夫张力滚蛋王五王八王五滚蛋都被乱七八糟的骂！";
+        //标记后的文本
+        String mark = trieTree.markWord(text, "【", "】");
+        System.out.println(mark);
     }
 
 }
