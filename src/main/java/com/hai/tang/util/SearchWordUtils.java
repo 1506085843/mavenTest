8ea9d5324a3189bd9a87dcfbbac213c559566cf6
xutao <1506085843@qq.com>
2023-12-01 17:39:50 +0800

    多文件搜索增加跳过的文本格式

--- src/main/java/com/hai/tang/util/SearchWordUtils.java
+++ src/main/java/com/hai/tang/util/SearchWordUtils.java
@@ -9,15 +9,19 @@ import java.util.*;
  */
 public class SearchWordUtils {
 
-    //当在文件夹中搜索时，遇到以下非文本文件则跳过
+    //当在文件夹中搜索时，遇到以下非文本格式的文件则跳过
     public static List<String> excludeFileType = Arrays.asList(
-            "jar", "zip", "rar", "7z", "tar", "gz", "xz", "bz2", "doc", "class", "pak",
-            "xls", "ppt", "pdf", "docx", "xlsx", "pptx", "jpg", "jpge", "gif", "png",
-            "xltd", "war", "hprof", "m4a", "swf", "mobi", "jpeg", "tiff", "svg", "psd",
+            "jar", "zip", "rar", "7z", "tar","gzip", "gz", "xz", "bz2", "doc", "class", "pak",
+            "xls", "ppt", "pdf","ofd", "docx", "xlsx", "pptx", "jpg", "jpge", "gif", "png",
+            "xltd", "war", "hprof", "m4a", "swf", "mobi", "jpeg","tif", "tiff", "svg", "psd","eps",
             "mp3", "aac", "mp4", "avi", "flv", "mkv", "mpeg", "msi", "tgz", "mdf",
             "rmvb", "apk", "ts", "map", "car", "mov", "wav", "raw", "dll", "woff",
             "eot", "otf", "ico", "ttf", "ttc", "fon", "dl_", "pd_", "ex_", "etl",
-            "sys", "iso", "isz", "esd", "wim", "gho", "dmg", "mpf", "exe", "ldf");
+            "sys", "iso", "isz", "esd", "wim", "gho", "dmg", "mpf", "exe", "ldf","xlsm","rm","ogg","mpg",
+            "wmv","3gp","drawio","dcm","tga","bmp","jfif","webp","dwg","dxf","dwf","iges","igs","dwt","dng",
+            "ifc","dwfx","stl","cf2","plt","obj","3ds","stl","ply","gltf","glb","off","3dm","fbx",
+            "dae","wrl","3mf","ifc","brep","step","iges","fcstd","bim","epub","wmf","emf","vsd","vsdx",
+            "xmind","odt","ods","ots","odp","otp","six","ott","fodt","fods","wps","dps","et","ett","wpt");
 
     /**
      * 搜索指定文件中的关键字
